2432498e8463e6c6317b9dd4d092f92b4cdab4e6
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2026-01-31 15:53:08 +0100

    Database structure (without relations)

---
+++ backend/models.py
@@ -0,0 +1,192 @@
+from sqlalchemy import Boolean, String, Integer, Float, TIMESTAMP ,Column, base
+from db import Base
+
+class user_login_credentials(Base):
+    __tablename__ = 'user_login_credentials'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        autoincrement= True,
+        unique= True,
+        default= '0',
+    )
+    email: str = Column(
+        String,
+        nullable= False,
+    )
+    hashed_password: str = Column(
+        String,
+        nullable= False,
+    )
+    created_at: TIMESTAMP = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+    policy_agreement: bool = Column(
+        Boolean,
+        nullable= False,
+    )
+    lastly_signed_in_on: TIMESTAMP = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+    role: int = Column(
+        Integer,
+        nullable= False,
+    )
+
+class schedule(Base):
+    __tablename__ = 'schedule'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        unique= True,
+    )
+    title: String = Column(
+        String,
+        nullable= False,
+    )
+    description: str = Column(
+        String,
+    )
+    created_by: int = Column(
+        Integer,
+        nullable= False,
+    )
+    created_at: str = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+    last_update_at: str = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+
+# TODO: Recurring Events
+class event(Base):
+    __tablename__ = 'event'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        unique= True,
+    )
+    title: str = Column(
+        String,
+        nullable= False,
+    )
+    description: set = Column(
+        String,
+        nullable= True,
+    )
+    from_time = ...
+    to_time = ...
+    from_date = ...
+    to_date = ...
+    entire_day: bool = Column(
+        Boolean,
+        nullable= True,
+    )
+    color: int = Column(
+        Integer,
+        nullable= False,
+    )
+    location: str = Column(
+        String,
+        nullable= True,
+    )
+    last_update_at: str = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+    created_by: int = Column(
+        Integer,
+        nullable= False,
+    )
+    created_at: str = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+
+class schedule_events(Base):
+    __tablename__ = 'schedule_events'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        unique= True,
+    )
+    id_schedule: int = Column(
+        Integer,
+        nullable= False,
+    )
+    id_event: int = Column(
+        Integer,
+        nullable= False,
+    )
+    created_at: str = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+
+class schedule_participants(Base):
+    __tablename__ = 'schedule_participants'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        unique= True,
+    )
+    id_schedule: int = Column(
+        Integer,
+        nullable= False,
+    )
+    id_user: int = Column(
+        Integer,
+        nullable= False,
+    )
+    role: str = Column(
+        Integer,
+        nullable= False,
+    )
+    created_at: str = Column(
+        TIMESTAMP,
+        nullable= False,
+    )
+
+class color(Base):
+    __tablename__ = 'color'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        unique= True,
+    )
+    name: str = Column(
+        String,
+        nullable= False,
+    )
+    hex_value: str = Column(
+        String,
+        nullable= False,
+    )
+
+class role(Base):
+    __tablename__ = 'role'
+    id: int = Column(
+        Integer,
+        primary_key= True,
+        nullable= False,
+        unique= True,
+    )
+    name: str = Column(
+        String,
+        nullable= False,
+    )
+    description: str = Column(
+        String,
+        nullable= True,
+    )
+
